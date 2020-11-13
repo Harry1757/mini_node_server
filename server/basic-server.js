ddd64aa8cbfa01f9e1353317a6009fe6705586da
황순환 <hwangsunhwan@Harry.local>
2020-11-13 18:36:44 +0900

    done

--- server/basic-server.js
+++ server/basic-server.js
@@ -5,12 +5,37 @@ const PORT = 5000;
 const ip = 'localhost';
 
 const server = http.createServer((request, response) => {
-  console.log(
-    `http request method is ${request.method}, url is ${request.url}`
-  );
-  response.writeHead(200, defaultCorsHeader);
-  response.end('hello mini-server sprints');
-});
+  if(request.method === 'OPTIONS'){
+    response.writeHead(200, defaultCorsHeader);
+    response.end()
+  }
+
+  if(request.method ==="POST" && request.url === '/upper'){
+    let body = []
+    request.on('data' , (chunk)=>{
+      body.push(chunk)
+    }).on('end',()=>{
+      body =Buffer.concat(body).toString().toUpperCase();
+      response.writeHead(200,defaultCorsHeader)
+      response.end(body);
+    })
+  }else if(request.method ==="POST" && request.url === '/lower'){
+    let body = []
+    request.on('data' , (chunk)=>{
+      body.push(chunk)
+    }).on('end',()=>{
+      body =Buffer.concat(body).toString().toLowerCase();
+      response.writeHead(200,defaultCorsHeader)
+      response.end(body);
+    })
+  }
+  else{
+    request.on('error',(err)=>{
+       response.writeHead(400,defaultCorsHeader)
+       console.error(err)
+    })
+  }
+})
 
 server.listen(PORT, ip, () => {
   console.log(`http server listen on ${ip}:${PORT}`);
@@ -22,3 +47,4 @@ const defaultCorsHeader = {
   'Access-Control-Allow-Headers': 'Content-Type, Accept',
   'Access-Control-Max-Age': 10
 };
+
